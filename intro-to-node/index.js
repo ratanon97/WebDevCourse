3faa17628ea9e73e4b8caaceee7b4cb2ec03ecb3
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-04-17 14:43:05 +0700

    Complete node js intro

---
+++ intro-to-node/index.js
@@ -0,0 +1,18 @@
+//jshint esversion6
+
+const fs = require("fs");
+
+//fs.copyFileSync('file1.txt', 'newFile.txt');
+
+// var superheroes = require("superheroes");
+
+// var mySuperHeroName = superheroes.random();
+
+// console.log(mySuperHeroName);
+
+
+var supervillains = require("supervillains");
+
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName);
