8c457a73bd65c30089006ca2a41ef3a28fa42e00
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-04-19 21:35:14 +0700

    Finish Body Parser API section

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,24 +1,38 @@
 const express = require("express");
-const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=ba46a91517551297827291564921a346&units=metric";
-    
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function(req,res){
+    const query = req.body.cityName;
+    const apiKey = "ba46a91517551297827291564921a346";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+
     https.get(url, function(response){
-        console.log(response.statusCode);
+         console.log(response.statusCode);
 
-        response.on("data",function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            console.log(weatherData);
-        })
-    });
-
-    res.send("Server is up and running!");
-})
+         response.on("data",function(data){
+             const weatherData = JSON.parse(data);
+             const temp = weatherData.main.temp;
+             const weatherDescription = weatherData.weather[0].description;
+             const icon = weatherData.weather[0].icon;
+             const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+             res.write("<p>The weather is currently " + weatherDescription + " </p>");
+             res.write("<h1>Current temperature " + query + " is " + temp + " degrees celsius!</h1>");
+             res.write("<img src="+imageURL+">");
+             res.send();
+         });
+     });
+});
 
 
 app.listen(3000, function(){
     console.log("Server Started On Port 3000");
-})
+});
