af0ddfd77f07c6aaf97eb3a350e871173596d4e4
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-04-22 21:14:36 +0700

    Complete CSS section

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,46 +4,29 @@ const https = require("https");
 
 const app = express();
 
-app.use("view engine","ejs");
+let items =["Buy Food","Cook Food","Eat Food"];
+
+app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("ERROR, CURRENT DAY IS EQUAL TO: " + currentDay);
-    }
+    let today = new Date();
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let day = today.toLocaleDateString("en-US", options);
     
-    res.render('list', {kindOfDay: day});
+    res.render('list', {kindOfDay: day, newListItems: items});
 
 });
 
+app.post("/",function(req,res){
+    let item = req.body.newItem;
+
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("Server Started On Port 3000");
