24c8e922fe8652be2992a47a7e8a8fb860de7517
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-05-13 20:40:27 +0700

    Complete Blog with Database Challenge

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true,useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://admin-ratanon:Tap45517!@cluster0-n0ayn.mongodb.net/todolistDB",{useNewUrlParser: true,useUnifiedTopology:true});
 
 const itemSchema = new mongoose.Schema ({
   name: {
